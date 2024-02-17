c72c8688f555d6fc8a9baef75a7b804e86b9e02e
alfariiizi <alfariiiziiiii@gmail.com>
2024-02-18 02:30:47 +0700

    feat(blog): posts list

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,19 +1,47 @@
 import { allPosts } from ".contentlayer/generated";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { Maxwidthdiv } from "../_components/Maxwindthdiv";
+import { displayFont } from "../_components/fonts";
+
+const formatter = new Intl.DateTimeFormat("en-US", { dateStyle: "long" });
 
 export default function Page() {
   console.log({ allPosts });
   return (
-    <Maxwidthdiv className="prose">
-      {allPosts.map((post) => (
-        <article key={post._id}>
-          <Link href={post.slug}>
-            <h2>{post.title}</h2>
-          </Link>
-          {post.description && <p>{post.description}</p>}
-        </article>
-      ))}
+    <Maxwidthdiv className="mt-10 flex justify-center">
+      <div className="flex w-full max-w-3xl flex-col gap-16">
+        <h2
+          className={cn(
+            "text-4xl font-bold text-primary lg:text-5xl",
+            displayFont.className,
+          )}
+        >
+          Latest posts &#9997;
+        </h2>
+        <div className={cn("flex flex-col gap-10", displayFont.className)}>
+          {allPosts.map((post) => (
+            <>
+              <article key={post._id} className="flex gap-5">
+                <p className="min-w-32 text-end text-secondary md:min-w-40">
+                  {formatter.format(new Date(post.date))}
+                </p>
+                <div className="flex flex-col gap-1">
+                  <Link
+                    href={post.slug}
+                    className="w-fit duration-150 hover:opacity-70"
+                  >
+                    <h3 className="text-xl font-semibold text-primary">
+                      {post.title}
+                    </h3>
+                  </Link>
+                  {post.description && <p>{post.description}</p>}
+                </div>
+              </article>
+            </>
+          ))}
+        </div>
+      </div>
     </Maxwidthdiv>
   );
 }
