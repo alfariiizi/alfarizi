063f1c4a8e20e551054d2fa7985b4ce42e9f7670
alfariiizi <alfariiiziiiii@gmail.com>
2024-03-08 21:56:32 +0700

    feat: client component in mdx

--- src/components/mdx/MDXComponets.tsx
+++ src/components/mdx/MDXComponets.tsx
@@ -1,3 +1,4 @@
+import CounterDemo from "@content/blog/CounterDemo";
 import { useMDXComponent } from "next-contentlayer/hooks";
 import Link from "next/link";
 import { Callout } from "./Callout";
@@ -5,12 +6,15 @@ import { Image } from "./Image";
 import { Math } from "./Math";
 import { elements } from "./basic-element";
 
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 const components = {
   Image,
   Callout,
   Link,
   Math,
   ...elements,
+  CounterDemo,
 };
 
 interface MdxProps {
