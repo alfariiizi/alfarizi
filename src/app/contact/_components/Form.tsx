c7693509cb2e721c6f57651f53add1ac08a93b6e
alfariiizi <alfariiiziiiii@gmail.com>
2024-03-09 13:09:51 +0700

    feat: add form component and use it in contact page

--- src/app/contact/_components/Form.tsx
+++ src/app/contact/_components/Form.tsx
@@ -1,5 +1,74 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Form as FormRoot,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  message: z.string(),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
 export default function Form() {
-  return <div>Form</div>;
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  function onSubmit(values: FormSchema) {
+    console.log(values);
+  }
+
+  return (
+    <FormRoot {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-5">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="someone@gmail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Message</FormLabel>
+              <FormControl>
+                <Textarea
+                  className="min-h-[8rem]"
+                  placeholder="I want to know more about you!"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </FormRoot>
+  );
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,12 +3,12 @@ import Form from "./_components/Form";
 
 export default function page() {
   return (
-    <Maxwidthdiv className="mt-10 flex max-w-5xl flex-col gap-10">
+    <Maxwidthdiv className="mt-10 flex max-w-3xl flex-col gap-10">
       <div className="flex flex-col gap-2">
         <h1 className="font-display text-3xl font-bold text-primary sm:text-4xl">
           Get in Touch 🫱🏼‍🫲🏽
         </h1>
-        <p className="max-w-3xl text-sm leading-normal sm:text-base">
+        <p className="text-sm leading-normal sm:text-base">
           Reach out to connect or collaborate with me. Whether you have
           questions, want to discuss a project, or simply want to say hello,
           feel free to send me an email.
