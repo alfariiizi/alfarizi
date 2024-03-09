6e0dc6b2400dfc15fff43cbb38859711ff09ff81
alfariiizi <alfariiiziiiii@gmail.com>
2024-03-09 12:15:58 +0700

    feat: integrate with shadcn-ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,65 +2,60 @@ import tailwindTypography from "@tailwindcss/typography";
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 import { layout, navbar } from "./src/app/_components/shared";
+import { colorsAlternative } from "./src/lib/colors-alternative";
 
 export default {
   content: ["./src/**/*.tsx"],
   darkMode: ["class"],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        foreground: "hsl(var(--foreground))",
+        ...colorsAlternative,
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
-        primary: "hsl(var(--primary))",
-        "primary-foreground": "hsl(var(--primary-foreground))",
-        secondary: "hsl(var(--secondary))",
-        "secondary-foreground": "hsl(var(--secondary-foreground))",
-        accent: "hsl(var(--accent))",
-        "accent-foreground": "hsl(var(--accent-foreground))",
-        "purple-alternative": {
-          100: "#e8e4f0",
-          200: "#d2cae1",
-          300: "#bbafd3",
-          400: "#a595c4",
-          500: "#8e7ab5",
-          600: "#726291",
-          700: "#55496d",
-          800: "#393148",
-          900: "#1c1824",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
-        "red-alternative": {
-          100: "#fcdfde",
-          200: "#fabfbe",
-          300: "#f79f9d",
-          400: "#f57f7d",
-          500: "#f25f5c",
-          600: "#c24c4a",
-          700: "#913937",
-          800: "#612625",
-          900: "#301312",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
-        "blue-alternative": {
-          100: "#d3e5ec",
-          200: "#a7cad9",
-          300: "#7cb0c6",
-          400: "#5095b3",
-          500: "#247ba0",
-          600: "#1d6280",
-          700: "#164a60",
-          800: "#0e3140",
-          900: "#071920",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
-        "yellow-alternative": {
-          100: "#fef3d5",
-          200: "#fde7ab",
-          300: "#fbda82",
-          400: "#face58",
-          500: "#f9c22e",
-          600: "#c79b25",
-          700: "#95741c",
-          800: "#644e12",
-          900: "#322709",
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
         },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       height: {
         "screen-without-navbar": `calc(100dvh - ${navbar.height} - ${layout.paddingTop} - ${layout.paddingBottom})`,
@@ -70,6 +65,22 @@ export default {
         display: ["var(--font-display)"],
         // display: ["var(--font-geist-sans)"],
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+
+      // [Typography]
       typography: {
         // customize typography
         // https://stackoverflow.com/questions/66594385/how-do-i-modify-the-default-styling-of-the-typography-prose-class-in-tailwindcss?newreg=53bd1cf2d09943f8b35b0528c1a0794c
