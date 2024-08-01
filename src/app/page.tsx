ad1617252aff407660e2f9a7e727e56895e9b220
Moh Rizal Alfarizi <alfariiiziiiii@gmail.com>
2024-08-01 13:38:02 +0700

    fix: hero section text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,32 +13,38 @@ export default function HomePage() {
     <Maxwidthdiv className="relative px-0">
       <div className="z-10 bg-background/60 backdrop-blur-2xl dark:bg-background/80 sm:backdrop-blur-3xl">
         <Maxwidthdiv className="mt-3 flex h-full flex-col items-center justify-end gap-12 sm:mt-0 sm:h-screen-without-navbar sm:pt-6 md:flex-row md:justify-between">
-          <div className="flex max-w-3xl flex-col gap-6 md:mb-28 md:gap-10">
-            <div className="flex flex-col-reverse gap-4 sm:flex-row">
-              <h1
-                className={cn(
-                  "font-display text-4xl font-black text-primary md:text-5xl md:leading-snug",
-                )}
-              >
-                Hey there &#128075; <br /> I&apos;m a{" "}
-                <span className="text-secondary">Cat lover</span>, Developer,
-                and Nerd.
-              </h1>
-              <div className="relative mx-auto !aspect-square h-[150px] w-[150px] overflow-clip rounded-full bg-primary p-2 shadow-lg sm:h-[120px]">
-                <Image
-                  src={ImgHero}
-                  alt="Alfarizi"
-                  placeholder="blur"
-                  width={200}
-                  height={200}
-                  className="absolute aspect-square"
-                  priority
-                  draggable={false}
-                />
+          <RoughNotationGroup show>
+            <div className="flex max-w-3xl flex-col gap-6 md:mb-28 md:gap-10">
+              <div className="flex flex-col-reverse gap-4 sm:flex-row">
+                <h1
+                  className={cn(
+                    "font-display text-4xl font-black text-primary md:text-5xl md:leading-snug",
+                  )}
+                >
+                  Hey there &#128075; <br /> I&apos;m{" "}
+                  <RoughNotation
+                    type="underline"
+                    color="hsl(var(--accent))"
+                    strokeWidth={2}
+                  >
+                    <span className="text-secondary">Rizal Alfarizi</span>
+                  </RoughNotation>
+                  , a Cat lover, Developer, and Nerd.
+                </h1>
+                <div className="relative mx-auto !aspect-square  h-[150px] overflow-clip rounded-full bg-primary p-2 shadow-lg sm:h-[120px]">
+                  <Image
+                    src={ImgHero}
+                    alt="Alfarizi"
+                    placeholder="blur"
+                    width={200}
+                    height={200}
+                    className="absolute aspect-square"
+                    priority
+                    draggable={false}
+                  />
+                </div>
               </div>
-            </div>
-            <div className="flex flex-col gap-6 leading-7">
-              <RoughNotationGroup show>
+              <div className="flex flex-col gap-6 leading-7">
                 <p>
                   24 years old nerd who interested in several IT fields,
                   including{" "}
@@ -86,9 +92,9 @@ export default function HomePage() {
                   as the primary graphics API in my personal projects within
                   Computer Graphics.
                 </p>
-              </RoughNotationGroup>
+              </div>
             </div>
-          </div>
+          </RoughNotationGroup>
           <div className="mb-14 flex flex-col items-end gap-2 place-self-end">
             <p>You can find me there &#128071;</p>
             <div className="flex items-center gap-3">
