3d5054f5b3f7db82feafe99f8384f8f8e5ea20d6
Moh Rizal Alfarizi <alfariiiziiiii@gmail.com>
2024-08-03 17:33:19 +0700

    feat(callout): style callout in mdx

--- src/components/mdx/Callout.tsx
+++ src/components/mdx/Callout.tsx
@@ -23,7 +23,8 @@ const defaultVariant: Variant = {
   status: "info",
   icon: IoInformationCircleOutline,
   defaultTitle: "Info",
-  className: "bg-teal-200 dark:bg-teal-900",
+  className:
+    "bg-teal-200 border-teal-800 dark:bg-teal-900 dark:border-teal-300",
 };
 
 const variants: Variant[] = [
@@ -32,25 +33,27 @@ const variants: Variant[] = [
     status: "warning",
     icon: IoWarningOutline,
     defaultTitle: "Warning",
-    className: "bg-yellow-100 dark:bg-yellow-900",
+    className:
+      "bg-yellow-100 border-yellow-800 dark:bg-yellow-900 dark:border-yellow-300",
   },
   {
     status: "note",
     icon: VscNotebook,
     defaultTitle: "Note",
-    className: "bg-sky-200 dark:bg-sky-900",
+    className: "bg-sky-200 border-sky-800 dark:bg-sky-900 dark:border-sky-300",
   },
   {
     status: "danger",
     icon: MdOutlineDangerous,
     defaultTitle: "Danger",
-    className: "bg-red-200 dark:bg-red-900",
+    className: "bg-red-200 border-red-800 dark:bg-red-900 dark:border-red-300",
   },
   {
     status: "key",
     icon: IoKeyOutline,
     defaultTitle: "Keypoint",
-    className: "bg-blue-200 dark:bg-blue-950",
+    className:
+      "bg-blue-200 border-blue-950 dark:bg-blue-950 dark:border-blue-200",
   },
 ] as const;
 
@@ -67,10 +70,7 @@ export function Callout({ title, children, status = "info" }: Props) {
   return (
     <AlertRoot
       variant="default"
-      className={cn(
-        "mb-8 mt-4 border-background shadow-lg shadow-gray-500 dark:shadow-gray-700",
-        variant.className,
-      )}
+      className={cn("mb-8 mt-4 border-2", variant.className)}
     >
       <AlertTitle
         className={cn(
