e02082eb1298cd0af7110af2b2c6b8eb9d74b591
Moh Rizal Alfarizi <alfariiiziiiii@gmail.com>
2026-05-05 21:50:14 +0700

    refactor: derive project collections from metadata

--- src/app/project/shared.ts
+++ src/app/project/shared.ts
@@ -1,19 +1,9 @@
-import { sortProjectsByStartDate } from "@/lib/content-utils.js";
+import {
+  getHighlightedProjects,
+  getNonHighlightedProjects,
+} from "@/lib/content-utils.js";
 import { projects } from ".velite/index";
 
-export const bookmarkProjectTitle = [
-  "[This Site] rizalalfarizi.com",
-  "Robota",
-  "Windsight",
-];
-
-export const sortedDateeProjects = sortProjectsByStartDate(projects);
-
-export const bookmarkProject = bookmarkProjectTitle.map(
-  (item) => projects.find((f) => f.title === item)!,
-);
-export const notBookmarkProject = sortedDateeProjects.filter((item) =>
-  bookmarkProjectTitle.every((s) => s !== item.title),
-);
-
-export const sortedProjects = [...bookmarkProject, ...notBookmarkProject];
+export const highlightedProjects = getHighlightedProjects(projects);
+export const nonHighlightedProjects = getNonHighlightedProjects(projects);
+export const sortedProjects = [...highlightedProjects, ...nonHighlightedProjects];
